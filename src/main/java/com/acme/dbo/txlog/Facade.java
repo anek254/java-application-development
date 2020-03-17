62b6182c81bcca64d5865f08435bf902f373c32f
Anya <boanya5@mail.ru>
2020-03-17 09:48:18 +0300

    iteration 1

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,11 +1,39 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
+    private static final String REFERENCE = "reference: ";
+    private static final String PRIMITIVE = "primitive: ";
+    private static final String STRING = "string: ";
+    private static final String CHAR = "char: ";
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        printMessage(PRIMITIVE, String.valueOf(message));
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        printMessage(PRIMITIVE, String.valueOf(message));
+    }
+
+
+    public static void log(String message) {
+        printMessage(STRING, message);
+    }
+
+    public static void log(boolean message) {
+        printMessage(PRIMITIVE, String.valueOf(message));
+    }
+
+
+    public static void log(Object message) {
+        printMessage(REFERENCE, String.valueOf(message));
+    }
+
+    public static void log (char message) {
+        printMessage(CHAR, String.valueOf(message));
+    }
+
+
+    private static void printMessage(String prefix, String message) {
+        System.out.println(prefix + message);
     }
 }
